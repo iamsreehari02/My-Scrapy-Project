a04d627951447f943d0623564d5a64b0bf8f6191
Sreehari <sreeharivnambiar72@gmail.com>
2022-11-08 15:55:06 +0530

    My Fourth Commit

--- demo1/spiders/myspider.py
+++ demo1/spiders/myspider.py
@@ -1,31 +1,32 @@
-# from scrapy.spiders import Spider
-import scrapy
+import scrapy 
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-class ScraperMe(scrapy.Spider):
-    name = 'spiderman'
+
+# https://www.bayut.com/to-rent/property/dubai/
+# https://www.bayut.com/property/details-6365453.html
+
+
+class MySpider(CrawlSpider):
+    name = 'sip'
+    allowed_domains = ['bayut.com']
     start_urls = ['https://www.bayut.com/to-rent/property/dubai/']
 
-    def parse(self, response):
-        for property in response.css('div.d6e81fd0'):
-            yield {
-                'property_id':  property.css('div._7afabd84::text').get(),
-                'price': 
-                        {
-                    'Currency' : property.css('span.c2cc9762::text').get(),
-                    'Amount' : property.css('span.f343d9ce::text').get()
-                        },
-                    
-                'property_type' :  property.css('div._9a4e3964::text').get(),
-                'bed_bath_size' : {
-                    'bed' : property.css('span.b6a29bc0::text')[0].get(),
-                    'bath' :property.css('span.b6a29bc0::text')[1].get() 
-                    # 'size' :                
-                     }, 
-                
-                }
-        next_page = response.css('a.b7880daf').attrib['href']
-        if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+    rules = (
+        Rule(LinkExtractor(allow='to-rent/property/dubai')),
+        Rule(LinkExtractor(allow='property/details',deny='ar/property'), callback='parse_item')
 
-            
+    )
 
+    def parse_item(self, response):
+        
+        yield {
+            'Property_id' :response.css('span._812aa185::text')[2].get(),
+            'Price': {
+                'Currency' : response.css('span.e63a6bfb::text').get(),
+                'Amount' : response.css('span._105b8a67::text').get()
+            },
+            'Location': response.css('div._1f0f1758::text').get(),
+            'Purpose': response.css('span._812aa185::text')[1].get(),
+            'Property_Type' : response.css('span._812aa185::text')[0].get()
+        }
